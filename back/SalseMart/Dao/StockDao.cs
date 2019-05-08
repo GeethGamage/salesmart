6f579397a2e11ed093078f21fb6d90e2d0642297
Arunodha Wijewantha <arunodha_w@epic.lk>
2019-05-08 15:16:33 +0530

    Change Backend Stock CRUD

---
+++ back/SalseMart/Dao/StockDao.cs
@@ -0,0 +1,47 @@
+﻿using SalseMart.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SalseMart.Dao
+{
+    public class StockDao
+    {
+        DBAccess _context;
+        public StockDao()
+        {
+            _context = DBAccess.Instance();
+        }   
+        public DataTable GetStock(int start,int limit)
+        {
+            string getStock = "select * from stock";
+            var getStockLimit = getStock + " LIMIT "+start+","+limit;
+            return _context.RunQuery(getStockLimit);
+        }
+        public void InsertStock(StockDto stockDto)
+        {            
+            string query = "INSERT INTO stock('CODE','NAME','WHOLESALE_PRICE','RETAIL_PRICE','ITEM_TYPE','IN_STOCK')" +
+                " values(" + stockDto.code + "," + stockDto.name + "," + stockDto.wholesalePrice + "," + stockDto.retailPrice + "," + stockDto.itemType + "," + stockDto.inStock + ")";
+            _context.RunNonQuery(query);            
+        } 
+        public void DeleteStock(int id)
+        {
+            string query = "Delete FROM stock where id = " + id;
+            _context.RunNonQuery(query);
+        }
+        public void UpdateStock(StockDto dto)
+        {
+            string query = "UPDATE stock SET CODE = "+dto.code
+                +"NAME = "+ dto.name
+                + "WHOLESALE_PRICE = " + dto.wholesalePrice
+                + "RETAIL_PRICE = " + dto.retailPrice
+                + "ITEM_TYPE = " + dto.itemType
+                + "IN_STOCK = " + dto.inStock
+                + "  where id = " + dto.id;
+            _context.RunNonQuery(query);
+        }
+        
+    }
+}
---
+++ back/SalseMart/Services/StockService.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SalseMart.Dao;
+using SalseMart.Dtos;
+using SalseMart.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SalseMart.Services
+{    
+    public class StockService
+    {
+        StockDao _stockDao;
+        public StockService()
+        {
+            _stockDao = new StockDao();
+        }       
+        public DataTableDTO GetStock(DataTableDTO dto)
+        {
+            var jsonObject = JObject.Parse(dto.dataTablesParameters);
+
+            var start = (int)jsonObject["start"];
+            var length = (int)jsonObject["length"];
+            DataTable dt = _stockDao.GetStock(start, length);
+            var dtoTo = new DataTableDTO();
+            dtoTo.dataList = new System.Collections.Generic.List<Object>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                var stock = new StockDto
+                {
+                    id = Convert.ToInt32(dr[0].ToString()),
+                    code = dr[1].ToString(),
+                    name = dr[2].ToString(),
+                    wholesalePrice = Convert.ToDecimal(dr[3].ToString()),
+                    retailPrice = Convert.ToDecimal(dr[4].ToString()),
+                    inStock = Convert.ToInt32(dr[5].ToString()),
+                    itemType = Convert.ToInt32(dr[6].ToString())
+                };
+                dtoTo.dataList.Add(stock);
+            }
+            return dtoTo;
+        }
+        public void InsertStock(string jsonString)
+        {
+            var item = JsonConvert.DeserializeObject<StockDto>(jsonString);
+            _stockDao.InsertStock(item);
+        }
+        public void DeleteStock(int id)
+        {
+            _stockDao.DeleteStock(id);
+        }
+        public void UpdateStock(string jsonString)
+        {
+            var item = JsonConvert.DeserializeObject<StockDto>(jsonString);
+            _stockDao.UpdateStock(item);
+        }
+    }
+}
